ca50946116320132d5656de906a864f4fbbf9f42
DaanWevers <210713@student.glu.nl>
2022-09-19 12:58:25 +0200

    .

--- main/pages/detail.php
+++ main/pages/detail.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>JURASSIC WORLD</title>
+    <link rel="stylesheet" href="/main/assets/css/detail.css">
 </head>
 <body>
 <?php
     include 'header.php'; 
-    ?>
+    ?>    
+
+
+
 </body>
 </html>
